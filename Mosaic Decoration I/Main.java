a17283be34b68e244b71703cfebbf091c2a9fb56
Haley <hs.haleyk@gmail.com>
2023-10-18 17:09:42 -0400

    update file

--- Mosaic Decoration I/Main.java
+++ Mosaic Decoration I/Main.java
@@ -1,10 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
         int n = keyboard.nextInt();
